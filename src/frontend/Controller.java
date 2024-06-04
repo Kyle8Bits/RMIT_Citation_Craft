c08aa4dd6b0b4c2025021b7d1af192998137aecf
Kyle8Bits <khoamaidang2611@gmail.com>
2024-06-04 16:43:20 +0700

    kai2

--- src/frontend/Controller.java
+++ src/frontend/Controller.java
@@ -9,32 +9,4 @@ import javafx.collections.ObservableList;
 
 public class Controller {
 
-    @FXML
-    private VBox mainContainer;
-
-    @FXML
-    public void initialize() {
-        addComboAndTextField();
-    }
-
-    @FXML
-    private void addComboAndTextField() {
-        ComboBox<String> comboBox = new ComboBox<>();
-        ObservableList<String> options = FXCollections.observableArrayList(
-            "Option 1", "Option 2", "Option 3", "Option 4"
-        );
-        comboBox.setItems(options);
-        
-        TextField textField = new TextField();
-        textField.setPromptText("Enter text");
-        
-        VBox additionalFields = new VBox(5.0);
-        comboBox.valueProperty().addListener((obs, oldVal, newVal) -> {
-            TextField newField = new TextField();
-            newField.setPromptText("Additional for " + newVal);
-            additionalFields.getChildren().add(newField);
-        });
-        
-        mainContainer.getChildren().addAll(comboBox, textField, additionalFields);
-    }
 }
